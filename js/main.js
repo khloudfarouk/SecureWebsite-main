a2a23d77c6975760db7b2d771e702cabcbe4f2c8
khloudfarouk <[khloudfarouk360@gmail.com]>
2025-08-21 00:09:29 +0300

    new changes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 
 
+  
 
 const contentItems = document.querySelectorAll('.content-item');
   const carouselElement = document.getElementById('imageCarousel');
 
   let currentIndex = 0;
   carouselElement.addEventListener('slide.bs.carousel', function (event) {
-    // إزالة active من المحتوى القديم
+    
     contentItems[currentIndex].classList.remove('active');
-    // تحديد المحتوى الجديد
+    
     currentIndex = event.to;
     contentItems[currentIndex].classList.add('active');
   });
@@ -18,7 +19,7 @@ const contentItems = document.querySelectorAll('.content-item');
 
 
 
-  // العناصر
+ 
 const cardIcons = document.querySelectorAll('.wwd-card-icon');
 const slidePreview = document.getElementById('wwd-slide-preview');
 const slideImg = document.getElementById('wwd-slide-img');
@@ -34,7 +35,6 @@ cardIcons.forEach(icon => {
 
     slidePreview.style.display = 'block';
 
-    // موقع السلايد حسب الماوس
     const rect = icon.getBoundingClientRect();
     slidePreview.style.top = rect.bottom + window.scrollY + 10 + 'px';
     slidePreview.style.left = rect.left + window.scrollX + 'px';
